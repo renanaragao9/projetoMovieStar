bd965919097856606a91e0800b16784ad9fe17c2
Renan Aragao <renan.aragao159@gmail.com>
2023-03-07 22:47:58 -0300

    Separação de templates

--- auth.php
+++ auth.php
@@ -1,10 +1,11 @@
 <?php
     require_once("templates/header.php");
 ?>
-    <div id="main-container" class="container-fluid">
-        <h1>Corpo do site</h1>
-    </div>
 
+    <div id="main-container" class="container-fluid">
+        <h1>Autenticação</h1>
+    </div>
+    
 <?php
     require_once("templates/footer.php");
 ?>
---
+++ templates/footer.php
@@ -0,0 +1,28 @@
+<footer id="footer" >
+        <div id="social-container">
+            <ul>
+                <li>
+                    <a href="#"><i class="fab fa-facebook-square"></i></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fab fa-youtube"></i></a>
+                </li>
+            </ul>
+        </div>
+        <div id="footer-links-container">
+            <ul>
+                <li><a href="#">Adicionar filme</a></li>
+                <li><a href="#">Adicionar crítica</a></li>
+                <li><a href="#">Entrar / Registrar</a></li>
+            </ul>
+        </div>
+        <p>&copy; 2023 Renan Aragão</p>
+    </footer>
+    <!-- BOOTSTRAP JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/js/bootstrap.js" integrity="sha512-KCgUnRzizZDFYoNEYmnqlo0PRE6rQkek9dE/oyIiCExStQ72O7GwIFfmPdkzk4OvZ/sbHKSLVeR4Gl3s7s679g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</body>
+</html>
