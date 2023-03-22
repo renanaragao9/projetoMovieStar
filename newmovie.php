2913fd19c6f467bf749a8d73f804ac01fc208f52
Renan Aragao <renan.aragao159@gmail.com>
2023-03-22 09:59:29 -0300

    Formulario de adição de filme

---
+++ newmovie.php
@@ -0,0 +1,52 @@
+<?php
+    require_once("templates/header.php");
+    require_once("models/User.php");
+    require_once("dao/UserDAO.php");
+    
+    // Verifica se usuario está autenticado
+    $user = new User();
+    $userDao = new UserDao($conn, $BASE_URL);
+    $userData = $userDao->verifyToken(true);
+?>
+    <div id="main-container" class="container-fluid">
+        <div class="offset-md-4 col-md-4 new-movie-container">
+            <h1 class="page-title">Adicionar Filme</h1>
+            <p class="page-description">Adicione sua crtítica e compartilhe com o mundo!</p>
+            <form action="<?= $BASE_URL ?>movie_process.php" id="add-movie-form" method="POST" enctype="multipart/form-data">
+                <input type="hidden" name="type" value="create">
+                <div class="form-group">
+                    <label for="title">Titulo:</label>
+                    <input type="text" class="form-control" id="title" name="title" placeholder="Digite o titulo do seu filme!">
+                </div>
+                <div class="form-group">
+                    <label for="image">Imagem:</label>
+                    <input type="file" class="form-control" id="image" name="image">
+                </div>
+                <div class="form-group">
+                    <label for="length">Duração:</label>
+                    <input type="text" class="form-control" id="length" name="length" placeholder="Digite a duração do filme">
+                </div>
+                <div class="form-group">
+                    <label for="category">Categoria:</label>
+                    <select name="category" class="form-control" id="category">
+                        <option value="">Selecione</option>
+                        <option value="Ação">Ação</option>
+                        <option value="Drama">Drama</option>
+                        <option value="Comédia">Comédia</option>
+                        <option value="Fantasia">Fantasia</option>
+                        <option value="Ficção">Ficção</option>
+                        <option value="Romance">Romance</option>
+                        <option value="Terror">Terror</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="description">Descrição:</label>
+                    <textarea name="description" class="form-control" id="description" rows="5" placeholder="Descreva o filme..."></textarea>
+                </div>
+            </form>
+        </div>
+    </div>
+
+<?php
+    require_once("templates/footer.php");
+?>
