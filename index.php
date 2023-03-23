94314ffb8f1e1b0ef7771152736d5fd1656e2b22
Renan Aragao <renan.aragao159@gmail.com>
2023-03-22 21:44:00 -0300

    Finalizando a home

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
     $movieDao = new MovieDAO($conn, $BASE_URL);
     
     $latestMovies = $movieDao->getLatestMovies(); 
-    $actionMovies = [];
-    $comedyMovies = [];
+    $actionMovies = $movieDao->getMoviesByCategory("Ação");
+    $comedyMovies = $movieDao->getMoviesByCategory("Comédia");
 
 ?>
     <div id="main-container" class="container-fluid">
@@ -17,16 +17,29 @@
             <?php foreach($latestMovies as $movie): ?>
                 <?php require("templates/movie_card.php"); ?>
             <?php endforeach; ?>
+            <?php if(count($latestMovies) === 0): ?>
+                <p class="empty-list">Ainda não há filmes cadastrados!</p>
+            <?php endif; ?>
         </div>
         <h2 class="section-title">Ação</h2>
         <p class="section-description">Veja os melhores filmes de ação</p>
         <div class="movies-container">
-            
+            <?php foreach($actionMovies as $movie): ?>
+                <?php require("templates/movie_card.php"); ?>
+            <?php endforeach; ?>
+            <?php if(count($actionMovies) === 0): ?>
+                <p class="empty-list">Ainda não há filmes de ação cadastrados!</p>
+            <?php endif; ?>
         </div>
         <h2 class="section-title">Comédia</h2>
         <p class="section-description">Veja os melhores filmes de comédia</p>
         <div class="movies-container">
-
+            <?php foreach($comedyMovies as $movie): ?>
+                <?php require("templates/movie_card.php"); ?>
+            <?php endforeach; ?>
+            <?php if(count($comedyMovies) === 0): ?>
+                <p class="empty-list">Ainda não há filmes de comédia cadastrados!</p>
+            <?php endif; ?>
         </div>
     </div>
 
