2134f53d3c675ffd36f2b7b9903bbd4a3064dfca
Renan Aragao <renan.aragao159@gmail.com>
2023-03-29 20:49:20 -0300

    Exibindo nota do filme na home

--- dao/MovieDAO.php
+++ dao/MovieDAO.php
@@ -2,8 +2,8 @@
 
     require_once("models/Movie.php");
     require_once("models/Message.php");
+    require_once("dao/ReviewDAO.php");
 
-    //Review DAO
 
     class MovieDAO implements MovieDAOInterface {
 
@@ -30,6 +30,13 @@
             $movie->category = $data["category"];
             $movie->length = $data["length"];
             $movie->users_id = $data["users_id"];
+
+            // Recebe as ratings do filme
+            $reviewDao = new ReviewDao($this->conn, $this->url);
+
+            $rating = $reviewDao->getRatings($movie->id);
+
+            $movie->rating = $rating;
 
             return $movie;
         }
