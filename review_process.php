7229c695c4af2bbdd8713b5881f273ddb65992e8
Renan Aragao <renan.aragao159@gmail.com>
2023-03-29 11:17:06 -0300

    Criando model da Review

---
+++ review_process.php
@@ -0,0 +1,21 @@
+<?php
+    require_once("globals.php");
+    require_once("db.php");
+    require_once("dao/UserDAO.php");
+    require_once("dao/MovieDAO.php");
+    require_once("models/Movie.php");
+    require_once("models/Message.php");
+
+    $message = new Message($BASE_URL);
+    $userDao = new UserDAO($conn, $BASE_URL);
+    $movieDao = new MovieDAO($conn, $BASE_URL);
+
+    // Receber o tipo do formulário
+    $type = filter_input(INPUT_POST, "type");
+
+    // Resgata dados do usuário
+    $userData = $userDao->verifyToken();
+
+    if($type === "create") {
+
+    }
